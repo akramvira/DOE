029d52a5886936297340be1aee11c5a16303b68f
akram <akram@jumbula.com>
2019-10-07 00:10:55 +0330

    update last

---
+++ src/app/views/admin/reports/performance-l3/web.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from "@angular/core";
+import { environment } from "../../../../../environments/environment";
+import { AuthenticationService } from "../../../../_services/authentication.service";
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WebService {
+  constructor(
+    private http: HttpClient,
+    private authServ: AuthenticationService
+  ) {}
+
+  public getExtensionsAndGroups() {
+    let options = this.authServ.getRequestOpions();
+    return this.http.get(environment.apiUrl + "/admin/reports/departments", options);
+  }
+
+  public getGroupPerformance(data) {
+    let options = this.authServ.getRequestOpions();
+      return this.http.post(
+        environment.apiUrl + "/admin/reports/chart/maingroup/filters",
+        data,
+        options
+      );
+  
+  }
+
+  public getNumbers(data){
+    let options = this.authServ.getRequestOpions();
+    return this.http.post(
+      environment.apiUrl + "/admin/reports/departments/filteritems/getnumbers",
+      data,
+      options
+    );
+  }
+
+}
