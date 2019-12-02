1a7fa0bf9008e5bcb1b96c16520786f1a833916f
akram <akram@jumbula.com>
2019-12-02 09:13:32 +0330

    build done

--- src/app/views/admin/_components/bar-chart/bar-chart.component.ts
+++ src/app/views/admin/_components/bar-chart/bar-chart.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from "@angular/core";
+import { Component, OnInit, Input, SimpleChanges } from "@angular/core";
 import { CustomTooltips } from "@coreui/coreui-plugin-chartjs-custom-tooltips";
 import { ExcelService } from "../../../../_services/excel.service";
 
@@ -14,14 +14,105 @@ export class BarChartComponent implements OnInit {
   @Input() isPercentChart: boolean = false;
   @Input() unitLabel: string = "";
   @Input() title: string = "";
+  @Input() timeType: string = "full"; // min , hour
 
   ngOnInit() {
+    this.setChartConfig();
+  }
+
+  public chartClicked(e: any): void {}
+
+  public chartHovered(e: any): void {
+    console.log(e);
+  }
+  // barChart1
+  @Input() datasets: Array<any> = [
+    {
+      data: [78, 81, 80],
+      label: "Series A"
+    }
+  ];
+  @Input() contentTitle: string = "";
+  @Input() labels: Array<any> = [];
+
+  public chartOptions: any;
+  @Input() colors: Array<any> = [
+    {
+      backgroundColor: "rgba(77, 189, 116, 1)",
+      borderColor: "rgba(77, 189, 116, 1)",
+      borderWidth: 1
+    },
+    {
+      backgroundColor: "#d0002c",
+      borderColor: "#d0002c",
+      borderWidth: 1
+    },
+    {
+      backgroundColor: "rgba(255, 193, 7, 1)",
+      borderColor: "rgba(255, 193, 7, 1)",
+      borderWidth: 1
+    },
+    {
+      backgroundColor: "rgba(255, 100, 50, 1)",
+      borderColor: "rgba(255, 193, 7, 1)",
+      borderWidth: 1
+    }
+  ];
+
+  @Input() chartType = "bar";
+
+  export(type: string = "excel") {
+    let tmpData: any = JSON.parse(JSON.stringify(this.datasets));
+    let data: any = [];
+    data.push({
+      اطلاعات: this.contentTitle
+    });
+
+    for (let it in tmpData) {
+      let record = {};
+      record["labels"] = tmpData[it]["label"];
+      for (let index in this.labels) {
+        record[this.labels[index].replace(/\s/g, "_")] =
+          tmpData[it]["data"][index]; // tmpData[it]['data'].pop();
+      }
+      data.push(record);
+    }
+
+    this.exportAsXLSX(data, type);
+  }
+
+  exportAsXLSX(data, type: string = "excel"): void {
+    this.excelService.exportAsExcelFile(data, this.chartType, type);
+  }
+
+  zoomOut() {}
+
+
+  ngOnChanges(changes: SimpleChanges) {
+    this.setChartConfig()
+}
+
+
+isZoomMode:boolean =false;
+
+setChartConfig(){
+  {
+
+    let barsCount = 0;
+    for(let i in this.datasets){
+      barsCount += this.datasets[i]['data'].length
+      debugger;
+    }
+
+    
+    
     let isTimeChart = this.isTimeChart;
     let isPercentChart = this.isPercentChart;
     let unitLabel = this.unitLabel;
 
     let topNumbersType = !isPercentChart? "value" : 'percent';
     let stepSizee = isPercentChart? 10: 1;
+    let timeType = this.timeType;
 
     this.chartOptions = {
       scaleShowVerticalLines: true,
@@ -118,11 +209,13 @@ export class BarChartComponent implements OnInit {
               fontColor: "black",
               fontSize: 11,
               padding:10,
+              margin:100,
               min: 0,
               maxTicksLimi:10,
               max: isPercentChart ? 100 : undefined,
               userCallback: function(item) {
                 if (isTimeChart) {
+                  if(item == 0) return 0;
                   let d = Number(item);
 
                   let day = Math.floor(d / (3600 * 24));
@@ -134,7 +227,14 @@ export class BarChartComponent implements OnInit {
                   let hDisplay = h >= 10 ? h : (("0" + h) as string);
                   let mDisplay = m >= 10 ? m : (("0" + m) as string);
                   let sDisplay = s >= 10 ? s : (("0" + s) as string); // > 0 ? s ;// + (s == 1 ? "" : "") : "";
-                  let time = hDisplay + ":" + mDisplay;// + ":" + sDisplay;
+
+                  let time = '';
+                  if(timeType == 'min')
+                     time =  mDisplay + ":" + sDisplay;
+                 else if(timeType == 'hour')
+                    time = hDisplay + ":" + mDisplay;// + ":" + sDisplay;
+                 else 
+                     time = hDisplay + ":" + mDisplay;// + ":" + sDisplay;
 
                   return time;
                 } else if (isPercentChart) return item + "%";
@@ -152,6 +252,8 @@ export class BarChartComponent implements OnInit {
         labels: {
           render: function (args) {
             
+            console.log('barsCount', barsCount);
+            if(!this.isZoomMode && barsCount > 10) return '';
             if(args.value == 0)
               return '';
 
@@ -210,71 +312,10 @@ export class BarChartComponent implements OnInit {
       }
     };
   }
+}
 
-  public chartClicked(e: any): void {}
+onCloseModal(){
+  debugger;
+}
 
-  public chartHovered(e: any): void {
-    console.log(e);
-  }
-  // barChart1
-  @Input() datasets: Array<any> = [
-    {
-      data: [78, 81, 80],
-      label: "Series A"
-    }
-  ];
-  @Input() contentTitle: string = "";
-  @Input() labels: Array<any> = [];
-
-  public chartOptions: any;
-  @Input() colors: Array<any> = [
-    {
-      backgroundColor: "rgba(77, 189, 116, 1)",
-      borderColor: "rgba(77, 189, 116, 1)",
-      borderWidth: 1
-    },
-    {
-      backgroundColor: "#d0002c",
-      borderColor: "#d0002c",
-      borderWidth: 1
-    },
-    {
-      backgroundColor: "rgba(255, 193, 7, 1)",
-      borderColor: "rgba(255, 193, 7, 1)",
-      borderWidth: 1
-    },
-    {
-      backgroundColor: "rgba(255, 100, 50, 1)",
-      borderColor: "rgba(255, 193, 7, 1)",
-      borderWidth: 1
-    }
-  ];
-
-  @Input() chartType = "bar";
-
-  export(type: string = "excel") {
-    let tmpData: any = JSON.parse(JSON.stringify(this.datasets));
-    let data: any = [];
-    data.push({
-      اطلاعات: this.contentTitle
-    });
-
-    for (let it in tmpData) {
-      let record = {};
-      record["labels"] = tmpData[it]["label"];
-      for (let index in this.labels) {
-        record[this.labels[index].replace(/\s/g, "_")] =
-          tmpData[it]["data"][index]; // tmpData[it]['data'].pop();
-      }
-      data.push(record);
-    }
-
-    this.exportAsXLSX(data, type);
-  }
-
-  exportAsXLSX(data, type: string = "excel"): void {
-    this.excelService.exportAsExcelFile(data, this.chartType, type);
-  }
-
-  zoomOut() {}
 }
--- src/app/views/admin/_components/line-chart/line-chart.component.ts
+++ src/app/views/admin/_components/line-chart/line-chart.component.ts
@@ -12,6 +12,7 @@ export class LineChartComponent implements OnInit {
   constructor(private excelService: ExcelService) {}
   chartHsteps = 50;
   @Input() isTimeChart = false;
+  @Input() title :string='';;
   
  
 
